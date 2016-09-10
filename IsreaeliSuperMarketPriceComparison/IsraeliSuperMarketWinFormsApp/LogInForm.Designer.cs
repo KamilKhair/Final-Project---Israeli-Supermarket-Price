f715b7ee1e5677e2952f9de24ba27c59b6a4e65a
Kamil Khair <kamilk@code-college.net>
2016-09-11 01:36:49 +0300

    11/9/16

--- IsreaeliSuperMarketPriceComparison/IsraeliSuperMarketWinFormsApp/LogInForm.Designer.cs
+++ IsreaeliSuperMarketPriceComparison/IsraeliSuperMarketWinFormsApp/LogInForm.Designer.cs
@@ -29,55 +29,14 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogInForm));
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
             this.logInButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.userNameTextBox = new System.Windows.Forms.TextBox();
-            this.passwordTextBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // logInButton
-            // 
-            this.logInButton.BackgroundImage = global::IsraeliSuperMarketWinFormsApp.Properties.Resources.login;
-            this.logInButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.logInButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.logInButton.Location = new System.Drawing.Point(569, 294);
-            this.logInButton.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.logInButton.Name = "logInButton";
-            this.logInButton.Size = new System.Drawing.Size(372, 103);
-            this.logInButton.TabIndex = 29;
-            this.logInButton.UseVisualStyleBackColor = true;
-            this.logInButton.Click += new System.EventHandler(this.logInButton_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::IsraeliSuperMarketWinFormsApp.Properties.Resources.compare;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(410, 385);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 30;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(917, 84);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(162, 32);
-            this.label1.TabIndex = 31;
-            this.label1.Text = ": שם משתמש";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(972, 203);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 32);
-            this.label2.TabIndex = 32;
-            this.label2.Text = ": סיסמה";
             // 
             // userNameTextBox
             // 
@@ -93,17 +52,58 @@
             this.passwordTextBox.Size = new System.Drawing.Size(321, 38);
             this.passwordTextBox.TabIndex = 34;
             // 
+            // logInButton
+            // 
+            this.logInButton.BackgroundImage = global::IsraeliSuperMarketWinFormsApp.Properties.Resources.login;
+            this.logInButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.logInButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logInButton.Location = new System.Drawing.Point(518, 294);
+            this.logInButton.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.logInButton.Name = "logInButton";
+            this.logInButton.Size = new System.Drawing.Size(372, 103);
+            this.logInButton.TabIndex = 35;
+            this.logInButton.UseVisualStyleBackColor = true;
+            this.logInButton.Click += new System.EventHandler(this.logInButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::IsraeliSuperMarketWinFormsApp.Properties.Resources.compare;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(410, 385);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 36;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(888, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 32);
+            this.label1.TabIndex = 37;
+            this.label1.Text = ": שם משתמש";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(943, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 32);
+            this.label2.TabIndex = 38;
+            this.label2.Text = ": סיסמה";
+            // 
             // LogInForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1150, 405);
-            this.Controls.Add(this.passwordTextBox);
-            this.Controls.Add(this.userNameTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.logInButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.userNameTextBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(1182, 493);
@@ -118,11 +118,11 @@
 
         #endregion
 
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
         private System.Windows.Forms.Button logInButton;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox userNameTextBox;
-        private System.Windows.Forms.TextBox passwordTextBox;
     }
 }
